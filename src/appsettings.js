824a59fa31577e2ee9b2c6a48cc809f2a2ea74f4
code-dump-here <tranduynhan240903@gmail.com>
2025-08-21 14:16:24 +0700

    modified:   src/appsettings.js

--- src/appsettings.js
+++ src/appsettings.js
@@ -4,7 +4,7 @@
 const appsettings = {
   apiBaseUrl: process.env.NODE_ENV === 'development' 
     ? "http://localhost:8080/api" 
-    : "https://booklibwithsubapi.azurewebsites.net/api"
+    : "hhttps://swd392booklibwithsubbe-production-1bea.up.railway.app/api"
 };
 
 export default appsettings;
