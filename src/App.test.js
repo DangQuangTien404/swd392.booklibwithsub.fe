4a1a3df53f5da7cf36f29d19b85567f3c3bf0d2f
DangQuangTien <dangquangtien16102002@gmail.com>
2025-08-21 14:24:53 +0700

    Revert "review-current-website-content"
    This reverts commit 011bfa65e3501a5547a506ef04edc608ebfda7b1, reversing
    changes made to 66e87109104fc8c331ebc4e95861b15ecb03b4ff.

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('shows loading fallback', () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
-  const loading = screen.getByText(/Loading/i);
-  expect(loading).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
